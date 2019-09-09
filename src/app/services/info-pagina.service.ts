e30c69bf7d5a4295d865728700228e568748849c
JonathanLex <tsu.jonathan.palacios@gmail.com>
2019-09-09 13:39:11 -0500

    Fin sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,15 +9,30 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
 
-    // Leer el archivo Json
-
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-          this.cargada = true;
-          this.info = resp;
-        });
+    this.cargarInfo();
+    this.cargarEquipo();
    }
+
+   private cargarInfo() {
+ // Leer el archivo Json
+
+ this.http.get('assets/data/data-pagina.json')
+ .subscribe( (resp: InfoPagina) => {
+   this.cargada = true;
+   this.info = resp;
+ });
+   }
+
+   private cargarEquipo() {
+     this.http.get('https://ionic-curso-c77c8.firebaseio.com/equipo.json')
+         .subscribe( (resp: any) => {
+          this.equipo = resp;
+         });
+   }
+
+   // https://ionic-curso-c77c8.firebaseio.com/equipo.json
 }
